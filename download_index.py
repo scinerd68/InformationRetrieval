0fbdc080191b4fcf2277d731aa77875aac8dc4a0
Thang <thangphan0168@gmail.com>
2025-04-07 20:54:57 +0700

    download scifact bge base vector index

--- download_index.py
+++ download_index.py
@@ -1,2 +1,5 @@
 from pyserini.search.lucene import LuceneSearcher
-searcher = LuceneSearcher.from_prebuilt_index('beir-v1.0.0-scifact.flat')
+
+# index_name = "beir-v1.0.0-scifact.flat"
+index_name = "beir-v1.0.0-scifact.bge-base-en-v1.5.hnsw"
+searcher = LuceneSearcher.from_prebuilt_index(index_name)
