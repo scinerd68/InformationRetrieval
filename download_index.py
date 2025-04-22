997351ef2aa27bacc965da64140998303586702a
Thang <thangphan0168@gmail.com>
2025-04-22 21:09:17 +0700

    download more index

--- download_index.py
+++ download_index.py
@@ -2,4 +2,6 @@ from pyserini.search.lucene import LuceneSearcher
 
 # index_name = "beir-v1.0.0-scifact.flat"
 index_name = "beir-v1.0.0-scifact.bge-base-en-v1.5.hnsw"
+index_name = "beir-v1.0.0-scifact.contriever"
+index_name = "beir-v1.0.0-scifact.bge-base-en-v1.5.flat"
 searcher = LuceneSearcher.from_prebuilt_index(index_name)
